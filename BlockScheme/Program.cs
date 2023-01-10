4c2a91a39c9abb95c8171d63545609e32a3672fe
stank <stankevich_dg@mail.ru>
2023-01-11 02:11:50 +0300

    Добавьте файлы проекта.

---
+++ BlockScheme/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    public static User[] userList;
+    static void Main(string[] args)
+    {
+        userList = new User[0];
+        Start();
+    }
+
+    public static void Start()
+    {
+        Console.WriteLine("Добрый день, введите ваш логин:");
+        string enterLogin = Console.ReadLine();
+        bool userTrue = UserCheck(enterLogin);
+        UserManager.UserIsInList(userList, userTrue, enterLogin);
+    }
+
+    static bool UserCheck(string userName)
+    {
+        foreach (var user in userList)
+        {
+             if (user.Login == userName)
+             {
+                 return true;
+             }  
+        }        
+        return false;
+    }
+}
+ 
---
+++ BlockScheme/UserManager.cs
@@ -0,0 +1,61 @@
+﻿class UserManager
+{
+    
+    public static void UserIsInList(User[] users, bool userTrue, string enterLogin)
+    {
+        if (userTrue)
+        {
+            int userDataBase = MassiveFounder(enterLogin, users);
+            Console.WriteLine("Здраствуйте, " + users[userDataBase].Name);
+            if (users[userDataBase].IsPremium)
+            {
+                Console.WriteLine("Добро пожаловать, у вас подключен премиум аккаунт, программа запущена");
+                Program.Start();
+            }
+            else
+            {
+                Ads.ShowAds();
+                Program.Start();
+            }
+        }
+        else CreateUser(enterLogin, users);
+    }
+    public static void CreateUser(string login, User[] users)
+    {
+        User[] newList = users;
+        users = new User[newList.Length + 1];
+        if (newList.Length > 0)
+        {
+            for (int i = 0; i < users.Length-1; i++)
+            {
+                users[i] = newList[i];
+            }
+        }
+        users[users.Length - 1] = new User();
+        users[users.Length - 1].Login = login;
+        Console.WriteLine("Введите своё имя");
+        users[users.Length - 1].Name = Console.ReadLine();        
+        Console.WriteLine("Хотите ли вы подключить премиум аккаунт? (Да/Нет)");
+        users[users.Length - 1].IsPremium = PremiumCheck(Console.ReadLine());
+        Program.userList = users;
+        Program.Start();
+    }
+
+    public static int MassiveFounder(string login, User[] users)
+    {
+        for(int i = 0; i < users.Length; i++)
+        {
+            if (users[i].Login == login)
+                return i;   
+        }
+        return -1;
+    }
+
+    static bool PremiumCheck(string answer)
+    {
+        if(answer == "да" || answer == "Да")
+            return true;
+        else return false;
+    }
+}
+ 
